46a3e0d7aef216f44a688cd70322482085449aca
Баадуу <baaduubn@gmail.com>
2023-12-05 19:00:52 +0800

    2 button

--- Page-Navigation-using-MVVM-main/Source Code/Page Navigation App/Page Navigation App/obj/Debug/net6.0-windows/App.g.i.cs
+++ Page-Navigation-using-MVVM-main/Source Code/Page Navigation App/Page Navigation App/obj/Debug/net6.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace Page_Navigation_App {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -73,7 +73,7 @@ namespace Page_Navigation_App {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
         public static void Main() {
             Page_Navigation_App.App app = new Page_Navigation_App.App();
             app.InitializeComponent();
