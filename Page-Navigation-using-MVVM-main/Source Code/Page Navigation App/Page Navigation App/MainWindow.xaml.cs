3f74fc1dda4124b7afb31509cc2cad1eaec0ed79
Баадуу <baaduubn@gmail.com>
2023-12-08 15:50:30 +0800

    update done

--- Page-Navigation-using-MVVM-main/Source Code/Page Navigation App/Page Navigation App/MainWindow.xaml.cs
+++ Page-Navigation-using-MVVM-main/Source Code/Page Navigation App/Page Navigation App/MainWindow.xaml.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Net;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Xml;
 using AutoUpdaterDotNET;
+using SharpCompress.Archives;
+using SharpCompress.Common;
 
 namespace Page_Navigation_App
 {
@@ -12,16 +18,42 @@ namespace Page_Navigation_App
         public MainWindow()
         {
             InitializeComponent();
-
-            // Set up AutoUpdater.NET
             AutoUpdater.Start("https://www.baaduu.me/version.xml");
             AutoUpdater.ShowSkipButton = false;
             AutoUpdater.ShowRemindLaterButton = false;
-            // Hook up event handlers
             AutoUpdater.CheckForUpdateEvent += AutoUpdaterOnCheckForUpdateEvent;
-            AutoUpdater.InstalledVersion = new Version("1.0.5.0"); // Set your current application version
+            AutoUpdater.InstalledVersion = new Version("0.0.2.0"); // Set your current application version
         }
-       
+        private string GetXmlContent(string xmlUrl)
+        {
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    return client.DownloadString(xmlUrl);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error downloading XML content: {ex.Message}");
+            }
+        }
+
+        private string GetDownloadLink(string xmlContent)
+        {
+            try
+            {
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(xmlContent);
+
+                XmlNode urlNode = xmlDoc.SelectSingleNode("//url");
+                return urlNode?.InnerText;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error extracting download link from XML: {ex.Message}");
+            }
+        }
         private void AutoUpdaterOnCheckForUpdateEvent(UpdateInfoEventArgs args)
         {
             if (args.Error == null)
@@ -31,55 +63,44 @@ namespace Page_Navigation_App
                     MessageBoxResult dialogResult;
                     if (args.Mandatory.Value)
                     {
-                        dialogResult = System.Windows.MessageBox.Show(
-                            $@"Шинэ хувилбар  {args.CurrentVersion} байна. Та {args.InstalledVersion} хувилбарыг ашиглаж байна. Энэ бол шаардлагатай шинэчлэлт юм. Програмыг шинэчлэхийн тулд OK дарна уу.",
-                            @"Шинэчлэх боломжтой", MessageBoxButton.OK, MessageBoxImage.Information);
+                        dialogResult = MessageBox.Show(
+                            $@"New version {args.CurrentVersion} is available. You are currently using version {args.InstalledVersion}. This is a mandatory update. Click OK to update.",
+                            @"Update Available", MessageBoxButton.OK, MessageBoxImage.Information);
                     }
                     else
                     {
-                        dialogResult = System.Windows.MessageBox.Show(
-                            $@"Шинэ хувилбар байна {args.CurrentVersion} байна. Та {args.InstalledVersion} хувилбарыг ашиглаж байна.Та програмаа одоо шинэчлэхийг хүсч байна уу?",
-                            @"Шинэчлэх боломжтой", MessageBoxButton.YesNo, MessageBoxImage.Information);
+                        dialogResult = MessageBox.Show(
+                            $@"New version {args.CurrentVersion} is available. You are currently using version {args.InstalledVersion}. Do you want to update?",
+                            @"Update Available", MessageBoxButton.YesNo, MessageBoxImage.Information);
                     }
-
-                    // Uncomment the following line if you want to show the standard update dialog instead.
-                     AutoUpdater.ShowUpdateForm(args);
 
                     if (dialogResult == MessageBoxResult.Yes || dialogResult == MessageBoxResult.OK)
                     {
                         try
                         {
-                            if (AutoUpdater.DownloadUpdate(args))
-                            {
-                                
-                                try
-                                {
-                                    MessageBox.Show("dsad");
-                                    //string filePath = @"C:\Program Files\Supergem\view";
-                                    //Process.Start(filePath);
-                                }
-                                catch (Exception ex)
-                                {
-                                    MessageBox.Show("sda meen");
-                                    //Console.WriteLine($"An error occurred: {ex.Message}");
-                                }
+                            // Get the download link from the XML
+                            string xmlContent = GetXmlContent("https://www.baaduu.me/version.xml");
+                            string downloadLink = GetDownloadLink(xmlContent);
 
+                            // Download and process the file
+                            if (!string.IsNullOrEmpty(downloadLink))
+                            {
+                                DownloadRarFile(downloadLink);
                             }
                             else
                             {
-                                MessageBox.Show("sda yah ged bgan");
+                                MessageBox.Show("Invalid XML format. Unable to get download link.");
                             }
                         }
                         catch (Exception exception)
                         {
-                            MessageBox.Show("sda meen");
                             MessageBox.Show(exception.Message, exception.GetType().ToString(), MessageBoxButton.OK, MessageBoxImage.Error);
                         }
                     }
                 }
                 else
                 {
-                    System.Windows.MessageBox.Show(@"Шинэчлэлт байхгүй байна. Дараа дахин оролдож үзнэ үү.", @"Шинэчлэлт байхгүй байна",
+                    MessageBox.Show(@"No updates available. Please check again later.", @"No Updates Available",
                         MessageBoxButton.OK, MessageBoxImage.Information);
                 }
             }
@@ -87,17 +108,116 @@ namespace Page_Navigation_App
             {
                 if (args.Error is WebException)
                 {
-                    System.Windows.MessageBox.Show(
-                        @"Шинэчлэх серверт холбогдоход асуудал гарлаа. Интернэт холболтоо шалгаад дараа дахин оролдоно уу.",
-                        @"Шинэчлэх шалгалт амжилтгүй боллоо", MessageBoxButton.OK, MessageBoxImage.Error);
+                    MessageBox.Show(
+                        @"Failed to connect to the update server. Please check your internet connection and try again later.",
+                        @"Update Check Failed", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
                 else
                 {
-                    System.Windows.MessageBox.Show(args.Error.Message, args.Error.GetType().ToString(), MessageBoxButton.OK,
+                    MessageBox.Show(args.Error.Message, args.Error.GetType().ToString(), MessageBoxButton.OK,
                         MessageBoxImage.Error);
                 }
             }
         }
+
+
+        private void DownloadRarFile(string url)
+        {
+            try
+            {
+                string downloadPath = @"C:\Downloads\YourApplicationArchive.rar"; // Set your desired download path
+                string extractPath = @"C:\Downloads\"; // Set your desired extraction path
+                // Ensure the directory exists, create it if it doesn't
+                string directory = Path.GetDirectoryName(downloadPath);
+                if (!Directory.Exists(directory))
+                {
+                    Directory.CreateDirectory(directory);
+                }
+                if (!Directory.Exists(extractPath))
+                {
+                    Directory.CreateDirectory(extractPath);
+                }
+                using (WebClient webClient = new WebClient())
+                {
+                    webClient.DownloadFile(url, downloadPath);
+
+                    // Check if the file exists after downloading
+                    if (File.Exists(downloadPath))
+                    {
+                        // File downloaded successfully
+                        MessageBox.Show("ZIP file downloaded successfully!");
+                        ExtractRarFileAndRunMsi(downloadPath, extractPath);
+                        // Here you can perform actions like extracting the contents if needed
+                        // Example: ExtractZipFile(downloadPath);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Failed to download ZIP file.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error downloading ZIP file: {ex.Message}");
+            }
+        }
+        private void ExtractRarFileAndRunMsi(string rarFilePath, string extractPath)
+        {
+            try
+            {
+                using (var archive = ArchiveFactory.Open(rarFilePath))
+                {
+                    foreach (var entry in archive.Entries)
+                    {
+                        if (!entry.IsDirectory)
+                        {
+                            entry.WriteToDirectory(extractPath, new ExtractionOptions()
+                            {
+                                ExtractFullPath = true,
+                                Overwrite = true
+                            });
+
+                            // Check if the entry is an MSI file
+                            if (Path.GetExtension(entry.Key).Equals(".msi", StringComparison.OrdinalIgnoreCase))
+                            {
+                                // Assuming there's only one MSI file in the archive
+                                string msiFilePath = Path.Combine(extractPath, entry.Key);
+
+                                // Start the extracted MSI file
+                                StartExtractedMsi(msiFilePath);
+                                return;  // Stop processing after finding the first MSI file
+                            }
+                        }
+                    }
+                }
+
+                MessageBox.Show("RAR file extracted successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error extracting RAR file: {ex.Message}");
+            }
+        }
+
+        private void StartExtractedMsi(string msiFilePath)
+        {
+            try
+            {
+                // Start the extracted MSI file using Process.Start
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = msiFilePath,
+                    UseShellExecute = true
+                });
+
+                MessageBox.Show("Extracted MSI file started successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error starting extracted MSI file: {ex.Message}");
+            }
+        }
+
 
         private void CloseApp_Click(object sender, RoutedEventArgs e)
         {
--- Page-Navigation-using-MVVM-main/Source Code/Page Navigation App/Page Navigation App/obj/Debug/net6.0-windows/App.g.i.cs
+++ Page-Navigation-using-MVVM-main/Source Code/Page Navigation App/Page Navigation App/obj/Debug/net6.0-windows/App.g.i.cs
@@ -60,7 +60,7 @@ namespace Page_Navigation_App {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/Sain games platform;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Sain games platform;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
