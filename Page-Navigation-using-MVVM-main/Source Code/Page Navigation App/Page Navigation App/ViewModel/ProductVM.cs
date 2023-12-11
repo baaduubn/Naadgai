5bffdebf338c968f3eece044ee16c73694baa478
Баадуу <baaduubn@gmail.com>
2023-12-11 16:59:23 +0800

    fix view edr

--- Page-Navigation-using-MVVM-main/Source Code/Page Navigation App/Page Navigation App/ViewModel/ProductVM.cs
+++ Page-Navigation-using-MVVM-main/Source Code/Page Navigation App/Page Navigation App/ViewModel/ProductVM.cs
@@ -8,16 +8,6 @@ namespace Page_Navigation_App.ViewModel
     class ProductVM : Utilities.ViewModelBase
     {
         private readonly PageModel _pageModel;
-        private ObservableCollection<string> _yourTestButtonCollection;
-
-        public ObservableCollection<string> YourTestButtonCollection
-        {
-            get { return _yourTestButtonCollection; }
-            set { _yourTestButtonCollection = value; OnPropertyChanged(); }
-        }
-
-        public ICommand YourTestCommand { get; set; } // You may need to implement this command logic
-
         public string ProductAvailability
         {
             get { return _pageModel.ProductStatus; }
@@ -28,27 +18,6 @@ namespace Page_Navigation_App.ViewModel
         {
             _pageModel = new PageModel();
             ProductAvailability = "Out of Stock";
-
-            // Initialize and populate the collection with 100 test strings
-            YourTestButtonCollection = new ObservableCollection<string>();
-            for (int i = 1; i <= 100; i++)
-            {
-                YourTestButtonCollection.Add($"Test Button {i}");
-            }
-
-            YourTestCommand = new RelayCommand(YourTestCommandExecute, YourTestCommandCanExecute);
-        }
-
-        // Implement your command logic here
-        private void YourTestCommandExecute(object parameter)
-        {
-            // Your command logic goes here
-        }
-
-        private bool YourTestCommandCanExecute(object parameter)
-        {
-            // Your command can execute logic goes here
-            return true; // Change this according to your requirements
         }
     }
 }
